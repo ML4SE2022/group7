93ed35e1b527238edb2528b57a5d93c1988e1eff
Eivind Kjosbakken <eivindkjosbakken@gmail.com>
2022-10-17 23:05:13 +0100

    Uploading script to reduce json size by half

---
+++ misc/reduceJsonSize.py
@@ -0,0 +1,18 @@
+import json
+
+
+def split_in_files(json_data, amount):
+    step = len(json_data) // amount
+    pos = 0
+    for i in range(amount - 1):
+        with open('output_file{}.json'.format(i+1), 'w') as file:
+            json.dump(json_data[pos:pos+step], file)
+            pos += step
+    # last one
+    with open('output_file{}.json'.format(amount), 'w') as file:
+        json.dump(json_data[pos:], file)
+
+
+f = open("../data/train_codesearchnet_7.json")
+json_data = json.load(f)
+split_in_files(json_data, 2)
