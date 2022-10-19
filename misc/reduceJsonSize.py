a01625cbbbf93c07a0abeef53e27f1960b052fb9
Eivind Kjosbakken <eivindkjosbakken@gmail.com>
2022-10-19 17:25:33 +0100

    updated reduceJsonSize.py to divide train set by 10, and also so it does not make all output files

--- misc/reduceJsonSize.py
+++ misc/reduceJsonSize.py
@@ -5,9 +5,8 @@ def split_in_files(json_data, amount):
     step = len(json_data) // amount
     pos = 0
     for i in range(amount - 1):
-        with open('output_file{}.json'.format(i+1), 'w') as file:
-            json.dump(json_data[pos:pos+step], file)
-            pos += step
+
+        pos += step
     # last one
     with open('output_file{}.json'.format(amount), 'w') as file:
         json.dump(json_data[pos:], file)
@@ -15,4 +14,4 @@ def split_in_files(json_data, amount):
 
 f = open("../data/train_codesearchnet_7.json")
 json_data = json.load(f)
-split_in_files(json_data, 2)
+split_in_files(json_data, 10)
