4cf888ee87a8ffab2cb2c5fbc7ed715af4f943c5
Eivind Kjosbakken <eivindkjosbakken@gmail.com>
2022-10-22 14:21:04 +0100

    changing reduceJsonSize to split into 12

--- misc/reduceJsonSize.py
+++ misc/reduceJsonSize.py
@@ -1,17 +1,16 @@
 import json
 
 
-def split_in_files(json_data, amount):
+def split_in_files_codesearchnet(json_data, amount):
     step = len(json_data) // amount
     pos = 0
     for i in range(amount - 1):
-
         pos += step
     # last one
-    with open('output_file{}.json'.format(amount), 'w') as file:
+    with open('train_codesearchnet_7_short.json'.format(amount), 'w') as file:
         json.dump(json_data[pos:], file)
 
 
 f = open("../data/train_codesearchnet_7.json")
 json_data = json.load(f)
-split_in_files(json_data, 10)
+split_in_files_codesearchnet(json_data, 12)
