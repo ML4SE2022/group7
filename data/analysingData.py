ec92ee664a91d7b668dc81c7a3a5803f30d30f5d
Eivind Kjosbakken <eivindkjosbakken@gmail.com>
2022-10-23 15:32:21 +0100

    adding script to check fraction of zeros in original vs trimmed data

---
+++ data/analysingData.py
@@ -0,0 +1,20 @@
+
+import pandas as pd
+
+dfFullData = pd.read_json("train_codesearchnet_7.json")
+
+dfLabel = dfFullData["label"]
+print(dfLabel.head())
+fullLength = len(dfFullData["label"])
+numZeros = (dfFullData["label"] == 0).sum()
+print("Fraction of zeros in full data:", numZeros/fullLength)
+
+#print("NUM 0 in original {} NUM 1 IN ORIGINAL {}")
+#print("NUM 0 in TRIMMED {} NUM 1 IN TRIMMED {}")
+
+
+dfTrimmedData = pd.read_json("train_codesearchnet_7_trimmed.json")
+
+fullLength = len(dfTrimmedData["label"])
+numZeros = (dfTrimmedData["label"] == 0).sum()
+print("Fraction of zeros in trimmed data:", numZeros/fullLength)
