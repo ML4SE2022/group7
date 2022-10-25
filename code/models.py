48adf2c64a63a664e14be55dca8bc7f528cb79e4
Eivind Kjosbakken <eivindkjosbakken@gmail.com>
2022-10-25 15:09:13 +0100

    updating different model configs to match with running on only cosqa

--- code/models.py
+++ code/models.py
@@ -1,3 +1,6 @@
+from transformers.modeling_utils import PreTrainedModel
+from torch.nn import CrossEntropyLoss, MSELoss
+import torch.nn.functional as F
 import torch
 import torch.nn as nn
 import torch
@@ -5,16 +8,13 @@ from torch.autograd import Variable
 import copy
 # from transformers.modeling_bert import BertLayerNorm
 BertLayerNorm = torch.nn.LayerNorm
-import torch.nn.functional as F
 
-from torch.nn import CrossEntropyLoss, MSELoss
 # from transformers import (WEIGHTS_NAME, AdamW, get_linear_schedule_with_warmup,
 #                           BertConfig, BertForMaskedLM, BertTokenizer,
 #                           GPT2Config, GPT2LMHeadModel, GPT2Tokenizer,
 #                           OpenAIGPTConfig, OpenAIGPTLMHeadModel, OpenAIGPTTokenizer,
 #                           RobertaConfig, RobertaModel, RobertaTokenizer,
 #                           DistilBertConfig, DistilBertForMaskedLM, DistilBertTokenizer)
-from transformers.modeling_utils import PreTrainedModel
 
 
 class Model(PreTrainedModel):
@@ -23,7 +23,7 @@ class Model(PreTrainedModel):
         self.encoder = encoder
         self.config = config
         self.tokenizer = tokenizer
-        self.mlp = nn.Sequential(nn.Linear(768*4, 768),
+        self.mlp = nn.Sequential(nn.Linear(768*5, 768),
                                  nn.Tanh(),
                                  nn.Linear(768, 1),
                                  nn.Sigmoid())
@@ -39,8 +39,8 @@ class Model(PreTrainedModel):
         if return_vec:
             return code_vec, nl_vec
 
-        logits = self.mlp(torch.cat((nl_vec, code_vec, nl_vec-code_vec, nl_vec*code_vec), 1))
+        logits = self.mlp(torch.cat(
+            (nl_vec, code_vec, nl_vec-code_vec, nl_vec*code_vec, nl_vec+code_vec), 1))
         loss = self.loss_func(logits, labels.float())
         predictions = (logits > 0.5).int()  # (Batch, )
         return loss, predictions
-
